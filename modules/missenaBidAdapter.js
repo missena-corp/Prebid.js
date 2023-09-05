92eecec0fdeb75bdf34d8ca37b3f4a23e8ab2f6b
Peter Damoc <pdamoc@gmail.com>
2023-09-05 16:49:29 +0300

    Missena Fast Bid updates

--- modules/missenaBidAdapter.js
+++ modules/missenaBidAdapter.js
@@ -1,11 +1,14 @@
-import { buildUrl, formatQS, logInfo, triggerPixel } from '../src/utils.js';
+import { formatQS, logInfo } from '../src/utils.js';
 import { BANNER } from '../src/mediaTypes.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { getStorageManager } from '../src/storageManager.js';
 
 const BIDDER_CODE = 'missena';
 const ENDPOINT_URL = 'https://bid.missena.io/';
 const EVENTS_DOMAIN = 'events.missena.io';
 const EVENTS_DOMAIN_DEV = 'events.staging.missena.xyz';
+
+export const storage = getStorageManager({ bidderCode: BIDDER_CODE });
 
 export const spec = {
   aliases: ['msna'],
@@ -30,6 +33,7 @@ export const spec = {
    * @return ServerRequest Info describing the request to the server.
    */
   buildRequests: function (validBidRequests, bidderRequest) {
+    tryGetMissenaFastBid();
     return validBidRequests.map((bidRequest) => {
       const payload = {
         adunit: bidRequest.adUnitCode,
@@ -61,6 +65,10 @@ export const spec = {
         payload.is_internal = bidRequest.params.isInternal;
       }
       payload.userEids = bidRequest.userIdAsEids || [];
+      if (window.MISSENA_FAST_BID && window.MISSENA_FAST_BID.cpm) {
+        payload.floor = window.MISSENA_FAST_BID.cpm;
+      }
+
       return {
         method: 'POST',
         url: baseUrl + '?' + formatQS({ t: bidRequest.params.apiKey }),
@@ -77,7 +85,25 @@ export const spec = {
    */
   interpretResponse: function (serverResponse, bidRequest) {
     const bidResponses = [];
-    const response = serverResponse.body;
+    let response = serverResponse.body;
+    if (typeof response !== 'object' || response === null) {
+      response = {};
+    }
+    const fastBid = window.MISSENA_FAST_BID;
+    const isFastBidHigher =
+      fastBid && (fastBid.cpm > response.cpm || !response.cpm);
+    if (isFastBidHigher) {
+      response.requestId = JSON.parse(bidRequest.data).request_id;
+      Object.assign(response, {
+        cpm: fastBid.cpm,
+        ad: '<script>window.top.__MISSENA__.renderFromBid(window.top.MISSENA_FAST_BID, "criteo-prebid-native")</script>',
+        creativeId: '',
+        currency: '',
+        dealId: '',
+        netRevenue: false,
+        ttl: 60,
+      });
+    }
 
     if (response && !response.timeout && !!response.ad) {
       bidResponses.push(response);
@@ -89,7 +115,7 @@ export const spec = {
     syncOptions,
     serverResponses,
     gdprConsent,
-    uspConsent
+    uspConsent,
   ) {
     if (!syncOptions.iframeEnabled) {
       return [];
@@ -123,16 +149,41 @@ export const spec = {
    */
   onBidWon: function (bid) {
     const hostname = bid.params[0].baseUrl ? EVENTS_DOMAIN_DEV : EVENTS_DOMAIN;
-    triggerPixel(
-      buildUrl({
-        protocol: 'https',
-        hostname,
-        pathname: '/v1/bidsuccess',
-        search: { t: bid.params[0].apiKey, provider: bid.meta?.networkName, cpm: bid.cpm, currency: bid.currency },
-      })
+    const body = {
+      name: 'bidsuccess',
+      provider: bid.meta?.networkName,
+      parameters: {
+        t: bid.params[0].apiKey,
+        commission: {
+          value: bid.cpm,
+          currency: bid.currency,
+        },
+      },
+    };
+    const headers = {
+      type: 'application/json',
+    };
+    const blob = new Blob([JSON.stringify(body)], headers);
+    navigator.sendBeacon(
+      `https://${hostname}/v1/events?t=${bid.params[0].apiKey}`,
+      blob,
     );
     logInfo('Missena - Bid won', bid);
   },
 };
+
+export function tryGetMissenaFastBid() {
+  try {
+    const fastBidStorageKey = 'msna_script';
+    const fastBidFromStorage =
+      storage.getDataFromLocalStorage(fastBidStorageKey);
+
+    if (fastBidFromStorage !== null) {
+      eval(fastBidFromStorage); // eslint-disable-line no-eval
+    }
+  } catch (e) {
+    // Unable to get fast bid
+  }
+}
 
 registerBidder(spec);
